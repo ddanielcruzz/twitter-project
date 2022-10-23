d0a0c3eb3ef25ce8f2b6e706f9462321ffe75ae1
Daniel Cruz <nox.sb@hotmail.com>
2022-10-23 11:32:11 -0500

    Improve ProfileToFollow styles

--- src/components/ProfileToFollow.jsx
+++ src/components/ProfileToFollow.jsx
@@ -2,13 +2,15 @@ import React from "react";
 
 export const ProfileToFollow = ({ profileAvatarURL, name, username }) => {
   return (
-    <article className="flex">
+    <article className="flex text-sm ">
       <img className="h-12 w-12 rounded-full" src={profileAvatarURL} alt="" />
       <section className="text-left">
         <p className="font-bold">{name}</p>
-        <p>{username}</p>
+        <p className="opacity-60">{username}</p>
       </section>
-      <button>Follow</button>
+      <button className="bg-white text-black font-semibold rounded-full px-4 py-1.5">
+        Follow
+      </button>
     </article>
   );
 };
